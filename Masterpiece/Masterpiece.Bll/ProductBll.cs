738b3e3a0d8078bf8d801b76be53ef0d65ff3967
GsQxZz <47713907+GsQxZz@users.noreply.github.com>
2019-12-31 18:00:21 +0800

    添加事务

--- Masterpiece/Masterpiece.Bll/ProductBll.cs
+++ Masterpiece/Masterpiece.Bll/ProductBll.cs
@@ -1,8 +1,12 @@
 ﻿using Masterpiece.Domain.Entity;
+using Masterpiece.Domain.Enum;
+using Masterpiece.Domain.MasterException;
+using Masterpiece.Domain.MasterResource;
 using Masterpiece.Repository.IRepository;
 using Masterpiece.Repository.Repository;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +36,33 @@ namespace Masterpiece.Bll
         public IList<Product> GetProducts()
         {
             return ProductRepository.GetAll();
+        }
+
+        public void Trans()
+        {
+            using (DbContextTransaction transaction = contextFactory.DBContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    BeginTran();
+                    if (true)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        throw new MasterAdvancedException<List<Product>>(ErrorCodeEnum.Wrong, MasterErrorMsg.ConstWrong);
+                    }
+                    transaction.Commit();
+                    CommitTran();
+                }
+                catch (Exception ex)
+                {
+                    RollbackTran();
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
         }
     }
 }
